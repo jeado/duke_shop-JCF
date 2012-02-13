ad087f384757bf46c855abe21388601e9c152553
disedu <disedu@disedu100.diseduzone.svc>
2012-02-13 16:51:59 +0900

    ggg

---
+++ duke_shop-JCF/src/main/java/com/dukeShop/model/Customer.java
@@ -0,0 +1,62 @@
+package com.dukeShop.model;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String passwd;
+	private String name;
+	private String email;
+
+	public Customer() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Customer(String userId, String password, String name, String email) {
+		super();
+		this.id = userId;
+		this.passwd = password;
+		this.name = name;
+		this.email = email;
+	}
+
+}
