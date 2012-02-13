029ed5a06e7cf40fb6162d0ee3d54843732655c2
disedu <disedu@disedu117.diseduzone.svc>
2012-02-13 21:14:59 +0900

    dd

---
+++ duke_shop-JCF/src/main/java/com/dukeShop/model/Product.java
@@ -0,0 +1,81 @@
+package com.dukeShop.model;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+		private String pid;
+		private String pname;
+		private String photo;
+		private String description;
+		private int price;
+		private int stock;
+
+		public Product (){}
+
+		public Product(String pid, String pname, String photo,
+				String description, int price, int stock) {
+			super();
+			this.pid = pid;
+			this.pname = pname;
+			this.photo = photo;
+			this.description = description;
+			this.price = price;
+			this.stock = stock;
+		}
+
+		public String getPid() {
+			return pid;
+		}
+
+		public void setPid(String pid) {
+			this.pid = pid;
+		}
+
+		public String getPname() {
+			return pname;
+		}
+
+		public void setPname(String pname) {
+			this.pname = pname;
+		}
+
+		public String getPhoto() {
+			return photo;
+		}
+
+		public void setPhoto(String photo) {
+			this.photo = photo;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+		public int getStock() {
+			return stock;
+		}
+
+		public void setStock(int stock) {
+			this.stock = stock;
+		}
+
+		public static long getSerialversionuid() {
+			return serialVersionUID;
+		}
+
+
+}
