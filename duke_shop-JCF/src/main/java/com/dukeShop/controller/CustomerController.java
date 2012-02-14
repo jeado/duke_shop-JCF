a26b5cedc7b094086a9668b250efc46728ed7cbf
burning29 <burning29@hanmail.net>
2012-02-14 14:47:10 +0900

    뭐지

---
+++ duke_shop-JCF/src/main/java/com/dukeShop/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.dukeShop.controller;
+
+import java.util.Map;
+
+import jcf.sua.mvc.MciRequest;
+import jcf.sua.mvc.MciResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dukeShop.model.Customer;
+import com.dukeShop.service.CustomerService;
+
+
+@Controller
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerSerivce;
+
+	@RequestMapping("join")
+	public void showJoinView(MciRequest mciRequest, MciResponse mciResponse){
+		mciResponse.setViewName("join_member");
+	}
+
+	@RequestMapping("JoinCustomer")
+	public void JoinMember(MciRequest mciRequest, MciResponse mciResponse){
+		Customer param = mciRequest.getParam(Customer.class);
+		System.out.println("환영합니다. : "+param.getName());
+
+		customerSerivce.insertCustomer(param);
+
+		mciResponse.set("member", param);
+		mciResponse.setViewName("join_member_proc");
+
+
+	}
+
+}
---
+++ duke_shop-JCF/src/main/java/com/dukeShop/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.dukeShop.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import jcf.sua.mvc.MciRequest;
+import jcf.sua.mvc.MciResponse;
+import jcf.upload.FileInfo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dukeShop.model.Product;
+import com.dukeShop.service.ProductService;
+
+@Controller
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+
+	@RequestMapping("index.action")
+	public void showAllProduct(MciRequest mciRequest,MciResponse mciResponse){
+		List<Product> allProduct = productService.getAllProduct();
+		mciResponse.setList("pp", allProduct);
+		mciResponse.setViewName("index");
+
+	}
+
+	@RequestMapping("insertProduct")
+	public void insertProduct(MciRequest mciRequest, MciResponse mciResponse){
+		Map<String, Object> param = mciRequest.getParam();
+		System.out.println(param);
+		mciResponse.setViewName("fileView");
+	}
+
+	@RequestMapping("viewFile")
+	public void vewFile(MciRequest mciRequest, MciResponse mciResponse){
+		mciResponse.setDownloadFile(new FileInfo("tempdir", "Chrysanthemum.jpg"));
+	}
+
+	@RequestMapping("showMeAFile")
+	public void show(MciRequest mciRequest, MciResponse mciResponse){
+		mciResponse.setViewName("fileView");
+	}
+
+}
